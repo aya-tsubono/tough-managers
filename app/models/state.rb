3dc19fc637e50a46d634c567230fdf3af4ba5c96
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2022-02-04 18:02:13 +0900

    修正

--- app/models/state.rb
+++ app/models/state.rb
@@ -5,9 +5,10 @@ class State < ActiveHash::Base
     { id: 3, name: '見学中' },
     { id: 4, name: '入居申込' },
     { id: 5, name: '入居決定' },
-    { id: 6, name: '保留' }
+    { id: 6, name: '保留' },
     { id: 7, name: '失注' }
   ]
 
   include ActiveHash::Associations
   has_many :cases
+end
