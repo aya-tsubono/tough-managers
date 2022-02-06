9912b55aabc7f2a6d8cedadd4b6a6b723e9f9018
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2022-02-06 19:43:05 +0900

    introducerモデルの作成

--- app/models/case.rb
+++ app/models/case.rb
@@ -2,7 +2,8 @@ class Case < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :state
+  belongs_to :introducer
 
   validates :state_id, presence: true, numericality: { only_integer: true }
-
+  validates :introducer_id, presence: true, numericality: { only_integer: true }
 end
---
+++ app/models/introducer.rb
@@ -0,0 +1,13 @@
+class Introducer < ActiveHash::Base
+  self.data = [
+    { id: 1, name: 'なし' },
+    { id: 2, name: '介護' },
+    { id: 3, name: '病院' },
+    { id: 4, name: '公的機関' },
+    { id: 5, name: '介護施設' },
+    { id: 6, name: '他' },
+  ]
+
+  include ActiveHash::Associations
+  has_many :cases
+end
