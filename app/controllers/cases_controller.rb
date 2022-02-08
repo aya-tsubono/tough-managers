97d1972a00e118ad5291c2b27bd6f5e1f12f12af
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2022-02-08 21:40:54 +0900

    createアクションの定義

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -9,7 +9,21 @@ class CasesController < ApplicationController
   end
 
   def create
+    @case = Case.new(case_params)
+    if @case.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
+
+  private
+
+  def case_params
+    params.require(:case).permit(:date, :state_id, :introducer_id, :introducer_name, :case_name, :age, :address, :household_id,
+                                :place_id, :economic_status_id, :care_level_id, :handicap_level, :cost, :location, :medical_condition,
+                                :care_condition, :remarks, :move_in_date, :main_fee, :other_fee, :home_name).merge(user_id: current_user.id)
   end
 
 end
