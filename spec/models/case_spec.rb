fcf7765b0f2f60d4f8e0d4ca87353edceeb204f2
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2022-02-09 16:10:18 +0900

    caseのテストモデルの作成

--- spec/models/case_spec.rb
+++ spec/models/case_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Case, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @case = FactoryBot.build(:case)
+  end
+
+  describe 'case登録' do
+    context '登録できる場合' do
+      it '必須項目を正しく入力すれば登録できる' do
+        expect(@case).to be_valid
+      end
+    end
+    context '登録できない場合' do
+      it 'dateが空では登録できない' do
+        @case.date = ''
+        @case.valid?
+        expect(@case.errors.full_messages).to include("Date can't be blank")
+      end
+    end
+  end
 end
