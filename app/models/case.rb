130f83cc02b163177ec592d7fb7b009e551689d0
aya-tsubono <88880199+aya-tsubono@users.noreply.github.com>
2022-02-13 00:25:14 +0900

    バリデーションの追加

--- app/models/case.rb
+++ app/models/case.rb
@@ -2,8 +2,16 @@ class Case < ApplicationRecord
   belongs_to :user
 
   validates :date, presence: true
-  validates :case_name, presence: true
-
+  validates :introducer_name, length: { maximum: 50 }
+  validates :case_name, presence: true, length: { maximum: 50 }
+  validates :age, length: { maximum: 50 }
+  validates :address, length: { maximum: 50 }
+  validates :handicap_level, length: { maximum: 500 }
+  validates :medical_condition, length: { maximum: 500 }
+  validates :care_condition, length: { maximum: 500 }
+  validates :cost, length: { maximum: 500 }
+  validates :location, length: { maximum: 500 }
+  validates :remarks, length: { maximum: 500 }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :state
